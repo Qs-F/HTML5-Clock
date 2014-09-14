8aa83ea194e715f846dffe1ef4ae985b9a0ca87e
Q'sF <qsf@mail.queensya.com>
2014-09-15 03:46:15 +0900

    Added from ChromeApps
    Added Files for ChromeApps

---
+++ main.js
@@ -0,0 +1,45 @@
+
+window.onload = setInterval(function showClock2 () {
+  var nowTime = new Date();
+  var month = set2fig( nowTime.getMonth()+1 );
+  var day = set2fig( nowTime.getDate() );
+  var nowHour = set2fig( nowTime.getHours() );
+  var nowMin = set2fig( nowTime.getMinutes() );
+  var nowSec = set2fig( nowTime.getSeconds() );
+  var days = month + "/" + day;
+  var msg = nowHour + ":" + nowMin + ":" + nowSec;
+  document.getElementById("RealtimeClockArea1").innerHTML = days;
+  document.getElementById("hours").innerHTML = nowHour;
+  document.getElementById("minutes").innerHTML = nowMin;
+  document.getElementById("seconds").innerHTML = nowSec;
+},1000);
+
+window.onload = setInterval(function() {
+  var backgroundId = 'back' + Math.floor( Math.random() * 12 ) + '.jpg';
+  document.getElementById("background").src = backgroundId;
+},60000);
+
+function set2fig(num) {
+   // 桁数が1桁だったら先頭に0を加えて2桁に調整する
+   var ret;
+   if( num < 10 ) { ret = "0" + num; }
+   else { ret = num; }
+   return ret;
+}
+
+
+
+/*function showClock2() {
+   var nowTime = new Date();
+   var month = set2fig( nowTime.getMonth()+1 );
+   var day = set2fig( nowTime.getDate() );
+   var nowHour = set2fig( nowTime.getHours() );
+   var nowMin = set2fig( nowTime.getMinutes() );
+   var nowSec = set2fig( nowTime.getSeconds() );
+   var days = month + "/" + day;
+   var msg = nowHour + ":" + nowMin + ":" + nowSec;
+   document.getElementById("RealtimeClockArea1").innerHTML = days;
+   document.getElementById("hours").innerHTML = nowHour;
+   document.getElementById("minutes").innerHTML = nowMin;
+   document.getElementById("seconds").innerHTML = nowSec;
+}*/
